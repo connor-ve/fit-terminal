b4e14f9cff9d74c0d2696492ee35fdf705bdb2e6
Connor Van Etten <87097190+Crvanetten15@users.noreply.github.com>
2024-05-02 11:48:18 -0400

    Patch : (inc) Adding Json DB

--- main.go
+++ main.go
@@ -1,11 +1,25 @@
 /*
 Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
 */
 package main
 
-import "xcute/fit-terminal/cmd"
+import (
+	"fmt"
+)
+
+type log struct {
+	Date    string `json:"date"`
+	Workout []struct {
+		Exercise int `json:exercise`
+		Set      struct {
+		} `json:set`
+	} `json:"workout"`
+}
 
 func main() {
-	cmd.Execute()
+	fmt.Println("Hello")
+}
+
+func pullWorkouts() {
+
 }
